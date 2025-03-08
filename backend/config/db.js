273cf8100034c43807fa40eca5f5c57cc4a810a1
Bhanu811 <22501a4410@pvpsit.ac.in>
2025-03-08 15:20:24 +0530

    project

---
+++ backend/config/db.js
@@ -0,0 +1,30 @@
+// const mongoose = require("mongoose")
+// require("dotenv").config()
+
+// const connectDB= async ()=>{
+//     try{
+//         await mongoose.connect(process.env.MONGODB_URL)
+//            .then(()=>{
+//                console.log("Mongodb connected succesfully")
+//            })
+//     }
+//     catch(err){
+//         console.log("Error while connecting with DB",err)
+//     }
+// }
+// module.exports=connectDB
+
+const mongoose = require("mongoose")
+require("dotenv").config()
+const connectDB= async ()=>{
+    try{
+        await mongoose.connect(process.env.MONGODB_URL)
+            .then(()=>{
+                console.log("Mongodb connected successfylly")
+            })
+    }
+    catch(err){
+        console.log("Error while connecting with DB", err)
+    }
+}
+module.exports=connectDB
